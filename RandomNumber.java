13221e5bbdf13fbfc3c2533c189e2d2d3cb08a85
Shashhwath <shashhwaths@gmail.com>
2023-12-30 20:41:34 +0530

    This is Raandom Number

--- RandomNumber.java
+++ RandomNumber.java
@@ -1,1 +1,49 @@
 This is RandomNumber.java
+
+
+package Task1;
+import java.util.*;
+public class RandomNumber 
+{
+public static void main(String[] args)
+{
+	Scanner z=new Scanner(System.in);
+  Random random=new Random();
+  int a=random.nextInt(100),b=1,s=0;
+  System.out.println("Random Number Generates From 0 to 99");
+  while(b<=5)
+  {
+	  System.out.println("Enter The Number:");
+	  int c=z.nextInt();
+	  if(c>a)
+	  {
+		  System.out.println("The Number is Too High");
+		  s++;
+	  }
+	  else if(c<a)
+	  {
+		  System.out.println("The Number is Too Low");
+		  s++;
+	  }
+	  else
+	  {
+		  System.out.println("Hurrah!! It is Right Answer");
+		  break;
+	  }
+	  b++;
+  }
+  int f=5-s;
+  
+  if(s==5)
+  {
+	  System.out.println("Sorry Better Luck Next Time!");
+  }
+  else
+  {
+	  System.out.println("Congrats!!! You Have Won The Game!");
+	  System.out.println("Your Score is "+f+"/5");
+  }
+  System.out.println("The Random Number Is "+a);
+}
+}
+
